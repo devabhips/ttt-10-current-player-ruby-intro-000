ccbd8905c765abe5a53be9bc6708120d1b0df181
dazzling-acrobat-7297 <abhips@gmail.com>
2020-02-11 12:06:56 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -22,5 +22,5 @@ def current_player(board)
   'O'
 end
 
-board = ['O', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ']
-puts turn_count(board).to_i
+board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+puts turn_count(board1).to_s
