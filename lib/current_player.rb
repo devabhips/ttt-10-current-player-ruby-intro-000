a914623f3eb29a25dd3e7556ae3c61febd0e3d5a
dazzling-acrobat-7297 <abhips@gmail.com>
2020-02-11 11:51:36 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
 
 def turn_count(board)
   count = 0
-  board.each {|pos| count += 1 if pos = 'X'|| pos == 'O'}
+  board.each { |pos| count += 1 if pos = 'X' || pos == 'O' }
+
+  count
 end
 
-def current_player()
-
-end
+def current_player; end
